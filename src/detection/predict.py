f27fdfc322bc53e81d6ea0b4b3e6509c67ea0a08
Parth Trivedi <parthtrivedi14092004@gmail.com>
2026-04-29 15:11:32 +0530

    Final training done

--- src/detection/predict.py
+++ src/detection/predict.py
@@ -1,21 +1,36 @@
 from ultralytics import YOLO
 import cv2
 
-# Load your trained model
-model = YOLO("runs/detect/train/weights/best.pt")
+# =========================
+# LOAD TRAINED MODEL
+# =========================
+model = YOLO(r"C:\Users\Parth Trivedi\Desktop\CODING\Deep Learning\driver-assistance-system\runs\detect\train\best.pt")
 
-# Load test image
-image_path = r"C:\Users\Parth Trivedi\Desktop\CODING\Deep Learning\driver-assistance-system\input.jpg"  
+# =========================
+# LOAD TEST IMAGE
+# =========================
+image_path = r"C:\Users\Parth Trivedi\Desktop\CODING\Deep Learning\driver-assistance-system\image.png"
 
 image = cv2.imread(image_path)
 
-# Run prediction
-results = model(image, conf=0.1)
-# Show results
+# =========================
+# RUN PREDICTION
+# =========================
+results = model(image, conf=0.25)
+
+# =========================
+# SHOW RESULTS
+# =========================
 results[0].show()
 
-# Save result
+# =========================
+# SAVE OUTPUT IMAGE
+# =========================
 results[0].save(filename="output.jpg")
+
+# =========================
+# PRINT DETECTIONS
+# =========================
 print(results[0].boxes)
 
 print("✅ Prediction Done! Check output.jpg")
